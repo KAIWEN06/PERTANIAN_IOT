b8bdf08cd58e842be6643fd38a0be5bb5926ab7a
AXRO98 <axroc98@gmail.com>
2026-06-18 03:23:28 +0800

    gateway node

--- src/gateway/main.cpp
+++ src/gateway/main.cpp
@@ -1,23 +1,35 @@
+// GATEWAY NODE
+
 #include "painlessMesh.h"
 
 #define MESH_PREFIX "BANANA_MESH"
 #define MESH_PASSWORD "12345678"
 #define MESH_PORT 5555
 
+#define NODE_NAME "GATEWAY"
+
 Scheduler userScheduler;
 painlessMesh mesh;
 
 void receivedCallback(uint32_t from, String &msg)
 {
-    Serial.print("Dari Node ");
-    Serial.print(from);
-    Serial.print(" : ");
+    Serial.println("================================");
+    Serial.print("Dari Mesh ID : ");
+    Serial.println(from);
+
+    Serial.print("Pesan        : ");
     Serial.println(msg);
+}
+
+void newConnectionCallback(uint32_t nodeId)
+{
+    Serial.print("Node Join : ");
+    Serial.println(nodeId);
 }
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(9600);
 
     mesh.init(
         MESH_PREFIX,
@@ -26,8 +38,11 @@ void setup()
         MESH_PORT);
 
     mesh.onReceive(receivedCallback);
+    mesh.onNewConnection(newConnectionCallback);
 
     Serial.println("Gateway Aktif");
+    Serial.print("Mesh ID Gateway : ");
+    Serial.println(mesh.getNodeId());
 }
 
 void loop()
